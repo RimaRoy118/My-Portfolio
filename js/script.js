bc6b131700b3438f74785413a864415943511fe1
Rima Roy <rimaroy.vawsum@gmail.com>
2026-01-11 21:56:26 +0530

    my skills sec add

--- js/script.js
+++ js/script.js
@@ -4,11 +4,32 @@ const links = document.querySelector(".links");
 menubar.addEventListener("click",()=>
 {
     links.classList.toggle("visible");
+
+    menubar.classList.toggle("fa-bars");
+    menubar.classList.toggle("fa-xmark");
 })
 
 Array.from(links.children).forEach(link => {
     link.addEventListener("click", () => {
         links.classList.remove("visible");
+    });
+});
+
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
     });
 });
 
